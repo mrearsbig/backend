24b619714ab163af03c2e5b79674811096f3933b
Alexander Viafara <mrearsbig@gmail.com>
2021-11-12 17:25:26 -0500

    Change model product

--- store/products/views/productadminview.py
+++ store/products/views/productadminview.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 from products.models import Product
 from products.serializers import ProductSerializer
@@ -7,9 +7,9 @@ from products.serializers import ProductSerializer
 class ProductAdminListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
 
 class ProductAdminRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
